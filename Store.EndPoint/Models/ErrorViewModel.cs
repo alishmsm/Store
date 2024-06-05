58ceffa062b0c42e6b8d77aa04b41e36e2c976a4
alishmsm <115838425+alishmsm@users.noreply.github.com>
2024-06-05 17:07:00 +0330

    Init Project

---
+++ Store.EndPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Store.EndPoint.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
